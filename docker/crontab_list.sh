b21e0903ee384e79b55ab77e586c2e720b124c60
HowieHye <howiehye@163.com>
2021-06-21 23:24:37 +0000

    Sync_Changes

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -1,4 +1,4 @@
-# 2021-06-21 12:37:04
+# 2021-06-21 23:23:21
 
 # 京豆变动通知
 0 20 * * * node /scripts/jd_bean_change.js >> /scripts/log/jd_bean_change.log 2&1
@@ -12,8 +12,6 @@
 2 8,12,18 * * * node /scripts/jd_bookshop.js >> /scripts/log/jd_bookshop.log 2&1
 # 京东汽车
 15 7 * * * node /scripts/jd_car.js >> /scripts/log/jd_car.log 2&1
-# 京东汽车兑换
-0 0 * * * node /scripts/jd_car_exchange.js >> /scripts/log/jd_car_exchange.log 2&1
 # 签到领现金
 5 0,6-23/4 * * * node /scripts/jd_cash.js >> /scripts/log/jd_cash.log 2&1
 # 领现金兑换红包
@@ -140,7 +138,5 @@
 50 0-23/8 * * * node /scripts/jd_wxj.js >> /scripts/log/jd_wxj.log 2&1
 # 省钱大赢家
 30 0,6-23/1 * * * node /scripts/jd_big_winner.js >> /scripts/log/jd_big_winner.log 2&1
-# 京享值PK
-10 0,6-23/4 * * * node /scripts/jd_ddo_pk.js >> /scripts/log/jd_ddo_pk.log 2&1
 # 超级无线组队分京豆
 15 6 * * 2 node /scripts/jd_z_shop_captain.js >> /scripts/log/jd_z_shop_captain.log 2&1
